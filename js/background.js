3fc3208afb2f76fd7fd0aba1c0c21d8aad1cd6bc
MohammadReza Mahmoodi <Saman@DESKTOP-GDO51AR>
2023-04-27 18:53:44 +0430

    add a function which generates a quiz of 10 words

--- js/background.js
+++ js/background.js
@@ -174,3 +174,106 @@ function getFromDB() {
     };
   });
 }
+
+async function getExampleForQuiz(word) {
+  let examples = [];
+
+  try {
+    const response = await fetch("https://www.ldoceonline.com/dictionary/" + word);
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const elements = doc.querySelectorAll(".cexa1g1.exa");
+
+    elements.forEach(element => {
+      examples.push(element.textContent);
+    });
+
+    // Generate a random index into the array
+    let randomIndex = Math.floor(Math.random() * examples.length);
+
+    // Select the item at the random index
+    return examples[randomIndex];
+  } catch (error) {
+    throw error;
+  }
+}
+
+function shuffleArray(array) {
+  array.sort(() => Math.random() - 0.5);
+  return array;
+}
+
+async function getFromDB2() {
+  return new Promise((resolve, reject) => {
+    // Open the database
+    const request = window.indexedDB.open("LexiDB", 1);
+
+    // Handle database open success
+    request.onsuccess = (event) => {
+      db = event.target.result;
+
+      // Open a read-only transaction on the savedWord object store
+      const transaction = db.transaction("savedWord", "readonly");
+      const objectStore = transaction.objectStore("savedWord");
+
+      // Get all objects in the object store
+      const getAllRequest = objectStore.getAll();
+
+      // Handle getAll success
+      getAllRequest.onsuccess = async (event) => {
+        // Get the first 10 items in the array
+        let data = event.target.result
+        shuffleArray(data)
+
+        let quizArr = data.slice(0, 10)
+        shuffleArray(data)
+        let quizOpt = data.slice(0, 10)
+        db.close()
+        db = null
+        let quizQuestions = []
+
+        try {
+          // Map each word to a promise that resolves with its example
+          const examplesArr = await Promise.all(quizArr.map(async (word) => {
+            const example = await getExampleForQuiz(word.word);
+            return example;
+          }));
+
+          for (let i = 0; i < 10; i++) {
+            const question = {
+              question: examplesArr[i],
+              answer: quizArr[i].word,
+              opt: quizOpt.slice(0, 3).map(item => item.word)
+            }
+            quizQuestions.push(question)
+            shuffleArray(quizOpt)
+          }
+
+          // Resolve the promise with the data
+          resolve(quizQuestions);
+        } catch (error) {
+          console.error(error);
+          reject(error);
+        }
+      };
+
+      // Handle getAll error
+      getAllRequest.onerror = (event) => {
+        db.close()
+        db = null
+        reject(event.target.error);
+      };
+    };
+
+    // Handle database open error
+    request.onerror = (event) => {
+      db.close()
+      db = null
+      reject(event.target.error);
+    };
+  });
+}
+// getFromDB2().then(result => {
+//   console.log(result)
+// })
